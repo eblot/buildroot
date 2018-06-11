3717da7468afaa29c31c4cf97bc23901ac73e43c
Emmanuel Blot <emmanuel.blot@free.fr>
2018-06-11 10:41:24 +0200

    Fix more issues w/ configuration and startup files

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -8,8 +8,7 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
     grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
     sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
-    grep -qE '^console::' ${TARGET_DIR}/etc/inittab || \
-    sed -i 's/^console:/#console:/' ${TARGET_DIR}/etc/inittab
+    sed -i 's/^console::/#console::/' ${TARGET_DIR}/etc/inittab
 fi
 
 if [ -e ${TARGET_DIR}/etc/init.d/S50sshd ]; then
@@ -21,19 +20,7 @@ fi
 
 if [ -e ${TARGET_DIR}/etc/fstab ]; then
     grep -qE '/config' ${TARGET_DIR}/etc/fstab || \
-    echo "/dev/mmcblk0p1   /config       ext4    rw,noauto   1   1" >> ${TARGET_DIR}/etc/fstab
-fi
-
-if [ -e ${TARGET_DIR}/etc/network/interfaces ]; then
-    grep -qE '^auto wlan0' ${TARGET_DIR}/etc/init.d/S50sshd || \
-    cat >>${TARGET_DIR}/etc/network/interfaces <<EOT
-
-auto wlan0
-iface wlan0 inet dhcp
-        pre-up wpa_supplicant -iwlan0 -c/etc/wpa_supplicant.conf -B
-        up iw wlan0 set power_save off
-        down killall wpa_supplicant
-EOT
+    echo "/dev/mmcblk0p2   /config       ext4    rw,noauto   1   1" >> ${TARGET_DIR}/etc/fstab
 fi
 
 cat >>${TARGET_DIR}/etc/ssh/sshd_config  <<EOT
@@ -42,27 +29,26 @@ PermitRootLogin yes
 PermitEmptyPasswords yes
 EOT
 
-cat >${TARGET_DIR}/etc/init.d/S91mpd  <<EOT
+cat >${TARGET_DIR}/etc/init.d/S95mpd  <<EOT
 #!/bin/sh
 
 # Sanity checks
 test -f /etc/mpd.conf || exit 0
 
 start() {
-    mkdir -p /run/mpd/music /run/mpd/playlists
-    mkdir -p /tmp/mpd
     printf "Starting mpd: "
-    start-stop-daemon --start --quiet --background --exec /usr/bin/mpd \
+    mkdir -p /run/mpd/music /run/mpd/playlists /tmp/mpd
+    start-stop-daemon --start --quiet --background --exec /usr/bin/mpd \\
         && echo "OK" || echo "FAIL"
 }
 
 stop() {
     printf "Stopping mpd: "
-    start-stop-daemon --stop --quiet --pidfile /var/run/mpd.pid \
+    start-stop-daemon --stop --quiet --pidfile /var/run/mpd.pid \\
         && echo "OK" || echo "FAIL"
 }
 
-case "$1" in
+case "\$1" in
     start)
         start
         ;;
@@ -75,11 +61,11 @@ case "$1" in
         start
         ;;
     *)
-        echo "Usage: $0 {start|stop|restart}"
+        echo "Usage: \$0 {start|stop|restart}"
         exit 1
 esac
 EOT
-chmod +x ${TARGET_DIR}/etc/init.d/S91mpd
+chmod +x ${TARGET_DIR}/etc/init.d/S95mpd
 
 cat >${TARGET_DIR}/etc/mpd.conf  <<EOT
 # Directory where the music is stored
@@ -110,27 +96,29 @@ bind_to_address      "localhost"
 bind_to_address      "/run/mpd/socket"
 EOT
 
-cat >${TARGET_DIR}/etc/init.d/S92inkradio  <<EOT
+cat >${TARGET_DIR}/etc/init.d/S96inkradio  <<EOT
 #!/bin/sh
 
 start() {
     printf "Starting inkradio: "
-    start-stop-daemon --start --background --verbose \
-            --pidfile /var/run/inkradio.pid \
-            --make-pidfile \
-            --exec /usr/bin/python3 /usr/local/bin/inkradio.py \
+    mkdir -p /run/mpd/playlists
+    cp /usr/local/inkradio/playlists/radios.m3u /run/mpd/playlists/
+    start-stop-daemon --start --background --verbose \\
+            --pidfile /var/run/inkradio.pid \\
+            --make-pidfile \\
+            --exec /usr/bin/python3 /usr/local/inkradio/python/radio.py \\
         && echo "OK" || echo "FAIL"
 }
 
 stop() {
     printf "Stopping inkradio: "
-    start-stop-daemon --stop \
-        --pidfile /var/run/inkradio.pid \
-        --remove-pidfile \
+    start-stop-daemon --stop \\
+        --pidfile /var/run/inkradio.pid \\
         && echo "OK" || echo "FAIL"
+    rm -f /var/run/inkradio.pid
 }
 
-case "$1" in
+case "\$1" in
     start)
         start
         ;;
@@ -143,13 +131,30 @@ case "$1" in
         start
         ;;
     *)
-        echo "Usage: $0 {start|stop|restart}"
+        echo "Usage: \$0 {start|stop|restart}"
         exit 1
 esac
 EOT
-chmod +x ${TARGET_DIR}/etc/init.d/S92inkradio
+chmod +x ${TARGET_DIR}/etc/init.d/S96inkradio
+
+cat >${TARGET_DIR}/etc/chrony.conf <<EOT
+dumpdir /run/chrony
+pool europe.pool.ntp.org iburst
+driftfile /run/chrony/drift
+makestep 1 3
+rtcsync
+EOT
+
+cat >${TARGET_DIR}/etc/default/chrony <<EOT
+CHRONY_ARGS="-4"
+EOT
 
 mkdir -p ${TARGET_DIR}/config
 rm -f ${BINARIES_DIR}/config.ext4
 ${HOST_DIR}/sbin/mkfs.ext4 -r 1 -N 0 -m 5 -L "config" -O ^64bit \
     ${BINARIES_DIR}/config.ext4 "2M"
+
+if [ -d "${TARGET_DIR}/etc/init.d/S49chrony" ] then;
+    # chrony may deadlock for some reason, so move it away
+    mv ${TARGET_DIR}/etc/init.d/S49chrony ${TARGET_DIR}/etc/init.d/S99chrony
+fi
