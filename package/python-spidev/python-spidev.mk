b30786e57d724f9647167a3cc67bab520b7ff89b
Emmanuel Blot <emmanuel.blot@free.fr>
2018-07-30 12:58:32 +0200

    Upgrade python spidev package for proper bytes support with Py3

--- package/python-spidev/python-spidev.mk
+++ package/python-spidev/python-spidev.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SPIDEV_VERSION = 3.2
-PYTHON_SPIDEV_SOURCE = spidev-$(PYTHON_SPIDEV_VERSION).tar.gz
-PYTHON_SPIDEV_SITE = https://pypi.python.org/packages/36/83/73748b6e1819b57d8e1df8090200195cdae33aaa22a49a91ded16785eedd
+PYTHON_SPIDEV_VERSION = 3.3
+# PYTHON_SPIDEV_SOURCE = py-spidev-$(PYTHON_SPIDEV_VERSION).tar.gz
+PYTHON_SPIDEV_SITE = $(call github,doceme,py-spidev,v$(PYTHON_SPIDEV_VERSION))
 PYTHON_SPIDEV_SETUP_TYPE = distutils
 PYTHON_SPIDEV_LICENSE = GPL-2.0
 PYTHON_SPIDEV_LICENSE_FILES = LICENSE.md
